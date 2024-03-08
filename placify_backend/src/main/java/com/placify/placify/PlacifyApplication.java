f4caee4e5463a4df51c4e437275323a2dc59d324
Anand S Kale <anandkaleask75@gmail.com>
2024-03-08 12:53:40 +0530

    Added Backend

---
+++ placify_backend/src/main/java/com/placify/placify/PlacifyApplication.java
@@ -0,0 +1,13 @@
+package com.placify.placify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacifyApplication.class, args);
+	}
+
+}
---
+++ placify_backend/src/test/java/com/placify/placify/PlacifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placify.placify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
