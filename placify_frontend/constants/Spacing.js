5808394fdab9881dab99694a2225de80afe5c2ce
Tatvam Rajyaguru <tvraj.2002@gmail.com>
2024-03-31 20:21:47 +0530

    some changes to responsiveness

--- placify_frontend/constants/Spacing.js
+++ placify_frontend/constants/Spacing.js
@@ -1,12 +1,19 @@
-import { responsiveWidth, responsiveHeight } from 'react-native-responsive-dimensions';
+// import { responsiveWidth, responsiveHeight } from 'react-native-responsive-dimensions';
 
-const Spacing = responsiveWidth(2.3); // Adjust this multiplier as needed
+// const Spacing = responsiveWidth(2.3); // Adjust this multiplier as needed
+
+// export default Spacing;
+
+
+
+
+import { responsiveHeight } from 'react-native-responsive-dimensions';
+
+// Calculate spacing based on device screen height
+const screenHeight = 892; // Height of Pixel 6 Pro in dp
+const Spacing = responsiveHeight(1) * (screenHeight / 892); // 1% of the screen height
 
 export default Spacing;
-
-
-
-
 
 
 
